778e3a4cd2ab58c67f99a7e8226590b926c01909
paralausalchemy <bulent.cetin@alchemy.com.tr>
2025-11-30 17:47:17 +0300

    İlk yükleme

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, jsonify
+from transformers import pipeline
+
+app = Flask(__name__)
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    message = data.get('message')
+    candidate_labels = data.get('candidateLabels', [])
+    result = classifier(message, candidate_labels)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
